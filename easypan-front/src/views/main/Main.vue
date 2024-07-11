948a6131d3ea736faf590aa8a4562ed954e7e85e
CegghnnoR <1564011300@qq.com>
2024-07-11 22:02:22 +0800

    文件

--- easypan-front/src/views/main/Main.vue
+++ easypan-front/src/views/main/Main.vue
@@ -43,12 +43,91 @@
       <!-- 导航 -->
       <div>全部文件</div>
     </div>
-    .file-list
+    <div class="file-list">
+      <Table
+        ref="dataTableRef"
+        :columns="columns"
+        :dataSource="tableData"
+        :fetch="loadDataList"
+        :initFetch="true"
+        :options="tableOptions"
+        @rowSelected="rowSelected"
+      >
+        <template #fileName="{ index, row }">
+          <div class="file-name">
+            {{ row.fileName }}
+          </div>
+        </template>
+      </Table>
+    </div>
   </div>
 </template>
 
 <script setup>
+// import Table from '@/components/Table.vue';
+// import { columns } from 'element-plus/es/components/table-v2/src/common';
+import { ref, reactive, getCurrentInstance, nextTick } from 'vue'
+const { proxy } = getCurrentInstance()
 
+const api = {
+  loadDataList: '/file/loadDataList',
+  rename: '/file/rename',
+  newFolder: '/file/newFolder',
+  getFolderInfo: '/file/getFolderInfo',
+  delFile: '/file/delFile',
+  changeFileFolder: '/file/changeFileFolder',
+  createDownloadUrl: '/file/createDownloadUrl',
+  download: '/api/file/download'
+}
+
+const columns = [
+  {
+    label: '文件名',
+    prop: 'filename',
+    scopedSlots: 'filename'
+  },
+  {
+    label: '修改时间',
+    prop: 'lastUpdateTime',
+    width: 200
+  },
+  {
+    label: '大小',
+    prop: 'fileSize',
+    scopedSlots: 'fileSize',
+    width: 200
+  }
+]
+
+const tableData = ref({})
+const tableOptions = ref({
+  extHeight: 50,
+  selectType: 'checkbox'
+})
+
+const fileNameFuzzy = ref()
+const category = ref()
+const loadDataList = async () => {
+  let params = {
+    pageNo: tableData.value.pageNo,
+    pageSize: tableData.value.pageSize,
+    fileNameFuzzy: fileNameFuzzy.value,
+    filePid: 0
+  }
+  if (params.category != 'all') {
+    delete params.filePid
+  }
+  let result = await proxy.Request({
+    url: api.loadDataList,
+    params: params
+  })
+  if (!result) {
+    return
+  }
+  tableData.value = result.data
+}
+
+const rowSelected = () => {}
 </script>
 
 <style lang="scss" scoped>
